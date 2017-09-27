cde8c80b2ab9ffa0a4c08bc85647cd87905db4c1
volgorean <volgorean@gmail.com>
2017-09-27 15:06:28 -0700

    added category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,23 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find(params["id"])
+    @category = Category.preload(:badges).find(params[:id])
+    
+    wishes = []
+    wishes = current_user.wishes.pluck(:badge_id) if current_user
+
+    @badges = []
+    @category.badges.group_by(&:collection).each do |collection, badges|
+      @badges << { collection: collection.name, badges: []}
+
+      badges.each do |badge|
+        @badges.last[:badges] << {
+          id: badge.id,
+          name: badge.name,
+          image: badge_image_path(badge),
+          link: badge_path(badge),
+          wish: (wishes.include? badge.id)
+        }
+      end
+    end
   end
 end
