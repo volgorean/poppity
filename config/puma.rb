8311ec5ef9aceca2170c334969ccad8120734da4
volgorean <volgorean@gmail.com>
2017-09-27 13:47:46 -0700

    reverted puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 # Change to match your CPU core count
-workers 1
+workers 2
 
 # Min and Max threads per worker
 threads 1, 6
@@ -8,25 +8,22 @@ app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
 # Default to production
-environment ENV.fetch("RAILS_ENV") { "production" }
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
 
-if Rails.env.production?
-  # Set up socket location
-  bind "unix://#{shared_dir}/sockets/puma.sock"
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/puma.sock"
 
-  # Logging
-  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-  # Set master PID and state locations
-  pidfile "#{shared_dir}/pids/puma.pid"
-  state_path "#{shared_dir}/pids/puma.state"
-  activate_control_app
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
 
-  on_worker_boot do
-    require "active_record"
-    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-  end
-else
-  plugin :tmp_restart
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
